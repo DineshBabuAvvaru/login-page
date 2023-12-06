c2af4a219dfd5b933382e8b7372874be62738447
DineshBabuAvvaru <102279771+DineshBabuAvvaru@users.noreply.github.com>
2023-12-06 12:38:39 +0530

    Login-Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./Login";
+import './App.css'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const { username, email, password } = data;
+
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (username !== "" && email !== "" && password !== "") {
+      alert("Form Submitted Successfully");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <center>
+          <Login />
+          <form autoComplete="off" onSubmit={submitHandler}>
+            <h2>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                onChange={changeHandler}
+                placeholder="Username"
+                className="username"
+                required
+              />{" "}
+              <br />
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={changeHandler}
+                placeholder="Email"
+                className="email"
+                required
+              />{" "}
+              <br />
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={changeHandler}
+                placeholder="Password"
+                className="password"
+                required
+              />{" "}
+              <br />
+              <input type="submit" name="submit" className="submit" /> <br />
+            </h2>
+          </form>
+        </center>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
